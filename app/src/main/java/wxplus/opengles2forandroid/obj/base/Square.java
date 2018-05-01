b4c430916ddbe45510c589d7106049b8aec6a765
wxdut <wxduter@gmail.com>
2018-05-01 14:11:36 +0800

    Correct two spellings.

--- app/src/main/java/wxplus/opengles2forandroid/obj/base/Square.java
+++ app/src/main/java/wxplus/opengles2forandroid/obj/base/Square.java
@@ -3,12 +3,12 @@ package wxplus.opengles2forandroid.obj.base;
 
 public class Square {
         public Point center;
-        public float widht;
+        public float width;
         public float height;
 
-        public Square(Point center, float widht, float height) {
+        public Square(Point center, float width, float height) {
             this.center = center;
-            this.widht = widht;
+            this.width = width;
             this.height = height;
         }
     }

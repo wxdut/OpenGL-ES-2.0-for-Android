675fb946a575b636331cf138c7fd9689024a8543
WangXiao <wxduter@gmail.com>
2018-05-03 19:29:18 +0800

    add ProjectionHelper

---
+++ app/src/main/java/wxplus/opengles2forandroid/OpenGL_04_Cube.java
@@ -0,0 +1,41 @@
+package wxplus.opengles2forandroid;
+
+import android.opengl.GLSurfaceView;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
+import static android.opengl.GLES20.GL_DEPTH_BUFFER_BIT;
+import static android.opengl.GLES20.glClear;
+import static android.opengl.GLES20.glClearColor;
+
+public class OpenGL_04_Cube extends BaseActivity {
+
+    @Override
+    public int layoutResId() {
+        return R.layout.activity_04_cube;
+    }
+
+    @Override
+    public GLSurfaceView.Renderer createGlViewRenderer() {
+        return new GLSurfaceView.Renderer() {
+            @Override
+            public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+            }
+
+            @Override
+            public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+            }
+
+            @Override
+            public void onDrawFrame(GL10 gl) {
+                glClearColor(1f, 1f, 1f, 1f);
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            }
+        };
+    }
+}
---
+++ app/src/main/java/wxplus/opengles2forandroid/utils/ProjectionHelper.java
@@ -0,0 +1,33 @@
+package wxplus.opengles2forandroid.utils;
+
+import android.opengl.Matrix;
+
+public class ProjectionHelper {
+
+    public static final int PROJECTION_MATRIX_LENGTH = 16;
+
+    public float[] modelMatrix;
+    public float[] viewMatrix;
+    public float[] projectionMatrix;
+
+    public ProjectionHelper() {
+        Matrix.setIdentityM(modelMatrix = new float[PROJECTION_MATRIX_LENGTH], 0);
+        Matrix.setIdentityM(viewMatrix = new float[PROJECTION_MATRIX_LENGTH], 0);
+        Matrix.setIdentityM(projectionMatrix = new float[PROJECTION_MATRIX_LENGTH], 0);
+    }
+
+    public float[] generateMvpMatrix() {
+        float[] vpMatrix = new float[PROJECTION_MATRIX_LENGTH];
+        Matrix.multiplyMM(vpMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+        float[] mvpMatrix = new float[PROJECTION_MATRIX_LENGTH];
+        Matrix.multiplyMM(mvpMatrix, 0,vpMatrix, 0, modelMatrix, 0);
+        return mvpMatrix;
+    }
+
+    public float[] generateVpMatrix() {
+        float[] vpMatrix = new float[PROJECTION_MATRIX_LENGTH];
+        Matrix.multiplyMM(vpMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+        return vpMatrix;
+    }
+
+}

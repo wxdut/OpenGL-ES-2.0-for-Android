7394d1690684646e0e3041b0edf5f7f8913432b7
wxdut <wxduter@gmail.com>
2018-05-24 09:45:52 +0800

    add libassimp.so

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,15 @@
 #include <jni.h>
 #include <string>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
 
 extern "C" JNIEXPORT jstring
 
 JNICALL
 Java_wxplus_opengles2forandroid_OpenGL_106_1Model_stringFromJNI(JNIEnv *env, jobject instance) {
     std::string hello = "Hello from C++";
+    Assimp::Importer importer;
+    const aiScene *scene = importer.ReadFile("", aiProcess_Triangulate | aiProcess_FlipUVs);
     return env->NewStringUTF(hello.c_str());
 }
